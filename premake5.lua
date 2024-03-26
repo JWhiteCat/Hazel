73b01fe7cc749a03ef85788b5c979a8f0aef3999
MAIN\wanj <1173653942@qq.com>
2024-03-26 22:59:09 +0800

    GLFW

--- premake5.lua
+++ premake5.lua
@@ -9,6 +9,11 @@ workspace "Hazel"
 
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
+IncludeDir = {}
+IncludeDir["GLFW"] = "Hazel/vendor/GLFW/include"
+
+include "Hazel/vendor/GLFW"
+
 project "Hazel"
 	location  "Hazel"
 	kind "SharedLib"
@@ -29,7 +34,15 @@ project "Hazel"
 	includedirs
 	{
 		"%{prj.name}/src",
-		"%{prj.name}/vendor/spdlog/include"
+		"%{prj.name}/vendor/spdlog/include",
+		"%{IncludeDir.GLFW}"
+	}
+	
+	links
+	{
+		"GLFW",
+		"opengl32.lib",
+		"dwmapi.lib"
 	}
 
 	filter "system:windows"
