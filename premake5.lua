7fb0a2f46e6b5aa65fd40e4287c1d079bda1c919
MAIN\wanj <1173653942@qq.com>
2024-03-31 00:52:37 +0800

    Added input polling.

--- premake5.lua
+++ premake5.lua
@@ -1,5 +1,6 @@
 workspace "Hazel"
 	architecture "x64"
+	startproject "Sandbox"
 	configurations
 	{
 		"Debug",
@@ -14,11 +15,12 @@ IncludeDir["GLFW"] = "Hazel/vendor/GLFW/include"
 IncludeDir["Glad"] = "Hazel/vendor/Glad/include"
 IncludeDir["ImGui"] = "Hazel/vendor/imgui"
 
-include "Hazel/vendor/GLFW"
-include "Hazel/vendor/Glad"
-include "Hazel/vendor/ImGui"
+group "Dependencies"
+    include "Hazel/vendor/GLFW"
+    include "Hazel/vendor/Glad"
+    include "Hazel/vendor/imgui"
 
-startproject "Sandbox"
+group ""
 
 project "Hazel"
 	location  "Hazel"
