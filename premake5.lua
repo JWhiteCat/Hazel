95aabcd18d9d9beca5053c4612c5d0ce0218fcb1
JWhiteCat <wanjian@njust.edu.cn>
2024-04-06 19:12:09 +0800

    Automated platform detection using predefined macro's

--- premake5.lua
+++ premake5.lua
@@ -81,7 +81,6 @@ project "Hazel"
 
 		defines
 		{
-			"HZ_PLATFORM_WINDOWS",
 			"HZ_BUILD_DLL",
 			"GLFW_INCLUDE_NONE"
 		}
@@ -132,11 +131,6 @@ project "Sandbox"
 
 	filter "system:windows"
 		systemversion "latest"
-
-		defines
-		{
-			"HZ_PLATFORM_WINDOWS"
-		}
 
 	filter "configurations:Debug"
 		defines "HZ_DEBUG"
