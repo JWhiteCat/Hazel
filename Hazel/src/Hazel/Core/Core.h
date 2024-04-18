c8789e1b203f34981ae9e6f151dec380b7f92b5d
JWhiteCat <wanjian@njust.edu.cn>
2024-04-18 22:43:12 +0800

    General cleanup + fix ResetStats

--- Hazel/src/Hazel/Core/Core.h
+++ Hazel/src/Hazel/Core/Core.h
@@ -4,31 +4,31 @@
 
 // Platform detection using predefined macros
 #ifdef _WIN32
-	/* Windows x64/x86 */
-	#ifdef _WIN64
-		/* Windows x64  */
-		#define HZ_PLATFORM_WINDOWS
-	#else
+/* Windows x64/x86 */
+#ifdef _WIN64
+/* Windows x64  */
+#define HZ_PLATFORM_WINDOWS
+#else
 		/* Windows x86 */
 		#error "x86 Builds are not supported!"
-	#endif
+#endif
 #elif defined(__APPLE__) || defined(__MACH__)
 	#include <TargetConditionals.h>
 	/* TARGET_OS_MAC exists on all the platforms
 	 * so we must check all of them (in this order)
 	 * to ensure that we're running on MAC
 	 * and not some other Apple platform */
-	#if TARGET_IPHONE_SIMULATOR == 1
+#if TARGET_IPHONE_SIMULATOR == 1
 		#error "IOS simulator is not supported!"
-	#elif TARGET_OS_IPHONE == 1
+#elif TARGET_OS_IPHONE == 1
 		#define HZ_PLATFORM_IOS
 		#error "IOS is not supported!"
-	#elif TARGET_OS_MAC == 1
+#elif TARGET_OS_MAC == 1
 		#define HZ_PLATFORM_MACOS
 		#error "MacOS is not supported!"
-	#else
+#else
 		#error "Unknown Apple platform!"
-	#endif
+#endif
 /* We also have to check __ANDROID__ before __linux__
  * since android is based on the linux kernel
  * it has __linux__ defined */
@@ -44,12 +44,22 @@
 #endif // End of platform detection
 
 #ifdef HZ_DEBUG
-	#define HZ_ENABLE_ASSERTS
+#if defined(HZ_PLATFORM_WINDOWS)
+#define HZ_DEBUGBREAK() __debugbreak()
+#elif defined(HZ_PLATFORM_LINUX)
+	#include <signal.h>
+	#define HZ_DEBUGBREAK() raise(SIGTRAP)
+#else
+	#error "Platform doesn't support debugbreak yet!"
+#endif
+#define HZ_ENABLE_ASSERTS
+#else
+	#define HZ_DEBUGBREAK()
 #endif
 
 #ifdef HZ_ENABLE_ASSERTS
-	#define HZ_ASSERT(x, ...) { if(!(x)) { HZ_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
-	#define HZ_CORE_ASSERT(x, ...) { if(!(x)) { HZ_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
+#define HZ_ASSERT(x, ...) { if(!(x)) { HZ_ERROR("Assertion Failed: {0}", __VA_ARGS__); HZ_DEBUGBREAK(); } }
+#define HZ_CORE_ASSERT(x, ...) { if(!(x)) { HZ_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); HZ_DEBUGBREAK(); } }
 #else
 	#define HZ_ASSERT(x, ...)
 	#define HZ_CORE_ASSERT(x, ...)
@@ -59,22 +69,23 @@
 
 #define HZ_BIND_EVENT_FN(fn) std::bind(&fn, this, std::placeholders::_1)
 
-namespace Hazel {
+namespace Hazel
+{
+    template <typename T>
+    using Scope = std::unique_ptr<T>;
 
-	template<typename T>
-	using Scope = std::unique_ptr<T>;
-	template<typename T, typename ... Args>
-	constexpr Scope<T> CreateScope(Args&& ... args)
-	{
-		return std::make_unique<T>(std::forward<Args>(args)...);
-	}
+    template <typename T, typename... Args>
+    constexpr Scope<T> CreateScope(Args&&... args)
+    {
+        return std::make_unique<T>(std::forward<Args>(args)...);
+    }
 
-	template<typename T>
-	using Ref = std::shared_ptr<T>;
-	template<typename T, typename ... Args>
-	constexpr Ref<T> CreateRef(Args&& ... args)
-	{
-		return std::make_shared<T>(std::forward<Args>(args)...);
-	}
+    template <typename T>
+    using Ref = std::shared_ptr<T>;
 
+    template <typename T, typename... Args>
+    constexpr Ref<T> CreateRef(Args&&... args)
+    {
+        return std::make_shared<T>(std::forward<Args>(args)...);
+    }
 }
--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -29,6 +29,7 @@ void Sandbox2D::OnUpdate(Hazel::Timestep ts)
 	m_CameraController.OnUpdate(ts);
 
 	// Render
+	Hazel::Renderer2D::ResetStats();
 	{
 		HZ_PROFILE_SCOPE("Renderer Prep");
 		Hazel::RenderCommand::SetClearColor({ 0.1f, 0.1f, 0.1f, 1 });
