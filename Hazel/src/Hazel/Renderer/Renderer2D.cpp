ebacc0dc550040d54bfb12f30bc5efed46c43d56
JWhiteCat <wanjian@njust.edu.cn>
2024-04-19 19:38:44 +0800

    Could not render more than Renderer2DData::MaxTextureSlots different textures

--- Hazel/src/Hazel/Renderer/Renderer2D.cpp
+++ Hazel/src/Hazel/Renderer/Renderer2D.cpp
@@ -218,6 +218,9 @@ namespace Hazel
 
         if (textureIndex == 0.0f)
         {
+            if (s_Data.TextureSlotIndex >= Renderer2DData::MaxTextureSlots)
+                FlushAndReset();
+            
             textureIndex = (float)s_Data.TextureSlotIndex;
             s_Data.TextureSlots[s_Data.TextureSlotIndex] = texture;
             s_Data.TextureSlotIndex++;
@@ -309,6 +312,9 @@ namespace Hazel
 
         if (textureIndex == 0.0f)
         {
+            if (s_Data.TextureSlotIndex >= Renderer2DData::MaxTextureSlots)
+                FlushAndReset();
+            
             textureIndex = (float)s_Data.TextureSlotIndex;
             s_Data.TextureSlots[s_Data.TextureSlotIndex] = texture;
             s_Data.TextureSlotIndex++;
