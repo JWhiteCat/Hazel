1a9795007a982b7ff8add09df49910c95843a32c
JWhiteCat <wanjian@njust.edu.cn>
2024-07-07 19:06:14 +0800

    Added Editor Camera

--- Hazel/src/Hazel/Scene/Scene.h
+++ Hazel/src/Hazel/Scene/Scene.h
@@ -1,9 +1,9 @@
 ﻿#pragma once
 
-#include "entt.hpp"
-
-
 #include "Hazel/Core/Timestep.h"
+#include "Hazel/Renderer/EditorCamera.h"
+
+#include "entt.hpp"
 
 namespace Hazel {
 
@@ -21,7 +21,8 @@ namespace Hazel {
         // TEMP
         entt::registry& Reg() { return m_Registry; }
 
-        void OnUpdate(Timestep ts);
+        void OnUpdateRuntime(Timestep ts);
+        void OnUpdateEditor(Timestep ts, EditorCamera& camera);
         void OnViewportResize(uint32_t width, uint32_t height);
 
         Entity GetPrimaryCameraEntity();
