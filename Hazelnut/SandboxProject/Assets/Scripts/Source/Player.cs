c209b2eb5abb3f2d2d30f1712a59a5139536653d
JWhiteCat <wanjian@njust.edu.cn>
2024-08-29 00:44:19 +0800

    Retrieving public C# Entity class fields
    - Also getting/setting values during runtime

--- Hazelnut/SandboxProject/Assets/Scripts/Source/Player.cs
+++ Hazelnut/SandboxProject/Assets/Scripts/Source/Player.cs
@@ -11,6 +11,9 @@ namespace Sandbox
     {
         private TransformComponent m_Transform;
         private Rigidbody2DComponent m_Rigidbody;
+        
+        public float Speed;
+        public float Time = 0.0f;
 
         void OnCreate()
         {
@@ -22,9 +25,10 @@ namespace Sandbox
 
         void OnUpdate(float ts)
         {
+            Time += ts;
             // Console.WriteLine($"Player.OnUpdate: {ts}");
 
-            float speed = 0.01f;
+            float speed = Speed;
             Vector3 velocity = Vector3.Zero;
 
             if (Input.IsKeyDown(KeyCode.W))
