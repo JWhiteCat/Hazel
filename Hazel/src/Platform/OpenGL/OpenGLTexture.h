830d7c12c355d6170abf0ac8da28cd40f2bfc345
JWhiteCat <wanjian@njust.edu.cn>
2024-09-06 01:15:57 +0800

    Generating texture atlas + displaying it in UI
    - Also added TextureSpecification with some params to Texture API

--- Hazel/src/Platform/OpenGL/OpenGLTexture.h
+++ Hazel/src/Platform/OpenGL/OpenGLTexture.h
@@ -4,37 +4,41 @@
 
 #include <glad/glad.h>
 
-namespace Hazel {
+namespace Hazel
+{
+    class OpenGLTexture2D : public Texture2D
+    {
+    public:
+        OpenGLTexture2D(const TextureSpecification& specification);
+        OpenGLTexture2D(const std::string& path);
+        virtual ~OpenGLTexture2D();
 
-	class OpenGLTexture2D : public Texture2D
-	{
-	public:
-		OpenGLTexture2D(uint32_t width, uint32_t height);
-		OpenGLTexture2D(const std::string& path);
-		virtual ~OpenGLTexture2D();
+        virtual const TextureSpecification& GetSpecification() const override { return m_Specification; }
 
-		virtual uint32_t GetWidth() const override { return m_Width;  }
-		virtual uint32_t GetHeight() const override { return m_Height; }
-		virtual uint32_t GetRendererID() const override { return m_RendererID; }
+        virtual uint32_t GetWidth() const override { return m_Width; }
+        virtual uint32_t GetHeight() const override { return m_Height; }
+        virtual uint32_t GetRendererID() const override { return m_RendererID; }
 
-		virtual const std::string& GetPath() const override { return m_Path; }
-		
-		virtual void SetData(void* data, uint32_t size) override;
+        virtual const std::string& GetPath() const override { return m_Path; }
 
-		virtual void Bind(uint32_t slot = 0) const override;
+        virtual void SetData(void* data, uint32_t size) override;
 
-		virtual bool IsLoaded() const override { return m_IsLoaded; }
+        virtual void Bind(uint32_t slot = 0) const override;
 
-		virtual bool operator==(const Texture& other) const override
-		{
-			return m_RendererID == other.GetRendererID();
-		}
-	private:
-		std::string m_Path;
-		bool m_IsLoaded = false;
-		uint32_t m_Width, m_Height;
-		uint32_t m_RendererID;
-		GLenum m_InternalFormat, m_DataFormat;
-	};
+        virtual bool IsLoaded() const override { return m_IsLoaded; }
 
+        virtual bool operator==(const Texture& other) const override
+        {
+            return m_RendererID == other.GetRendererID();
+        }
+
+    private:
+        TextureSpecification m_Specification;
+
+        std::string m_Path;
+        bool m_IsLoaded = false;
+        uint32_t m_Width, m_Height;
+        uint32_t m_RendererID;
+        GLenum m_InternalFormat, m_DataFormat;
+    };
 }
