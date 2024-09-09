5d6734564d0c561438f75b6b04343b688626d296
JWhiteCat <wanjian@njust.edu.cn>
2024-09-09 16:20:20 +0800

    Exposed TextComponent to C#
    - Added Vector4 and some script utils

--- Hazel-ScriptCore/Source/InternalCalls.cs
+++ Hazel-ScriptCore/Source/InternalCalls.cs
@@ -5,6 +5,8 @@ namespace Hazel
 {
     public static class InternalCalls
     {
+        #region Entity
+
         [MethodImplAttribute(MethodImplOptions.InternalCall)]
         internal extern static bool Entity_HasComponent(ulong entityID, Type componentType);
 
@@ -14,11 +16,19 @@ namespace Hazel
         [MethodImplAttribute(MethodImplOptions.InternalCall)]
         internal extern static object GetScriptInstance(ulong entityID);
 
+        #endregion
+
+        #region TransformComponent
+
         [MethodImplAttribute(MethodImplOptions.InternalCall)]
         internal extern static void TransformComponent_GetTranslation(ulong entityID, out Vector3 translation);
 
         [MethodImplAttribute(MethodImplOptions.InternalCall)]
         internal extern static void TransformComponent_SetTranslation(ulong entityID, ref Vector3 translation);
+
+        #endregion
+
+        #region Rigidbody2DComponent
 
         [MethodImplAttribute(MethodImplOptions.InternalCall)]
         internal extern static void Rigidbody2DComponent_ApplyLinearImpulse(ulong entityID, ref Vector2 impulse,
@@ -37,7 +47,41 @@ namespace Hazel
         internal extern static void Rigidbody2DComponent_ApplyLinearImpulseToCenter(ulong entityID, ref Vector2 impulse,
             bool wake);
 
+        #endregion
+
+        #region TextComponent
+
+        [MethodImplAttribute(MethodImplOptions.InternalCall)]
+        internal extern static string TextComponent_GetText(ulong entityID);
+
+        [MethodImplAttribute(MethodImplOptions.InternalCall)]
+        internal extern static void TextComponent_SetText(ulong entityID, string text);
+
+        [MethodImplAttribute(MethodImplOptions.InternalCall)]
+        internal extern static void TextComponent_GetColor(ulong entityID, out Vector4 color);
+
+        [MethodImplAttribute(MethodImplOptions.InternalCall)]
+        internal extern static void TextComponent_SetColor(ulong entityID, ref Vector4 color);
+
+        [MethodImplAttribute(MethodImplOptions.InternalCall)]
+        internal extern static float TextComponent_GetKerning(ulong entityID);
+
+        [MethodImplAttribute(MethodImplOptions.InternalCall)]
+        internal extern static void TextComponent_SetKerning(ulong entityID, float kerning);
+
+        [MethodImplAttribute(MethodImplOptions.InternalCall)]
+        internal extern static float TextComponent_GetLineSpacing(ulong entityID);
+
+        [MethodImplAttribute(MethodImplOptions.InternalCall)]
+        internal extern static void TextComponent_SetLineSpacing(ulong entityID, float lineSpacing);
+
+        #endregion
+
+        #region Rigidbody2DComponent
+
         [MethodImplAttribute(MethodImplOptions.InternalCall)]
         internal extern static bool Input_IsKeyDown(KeyCode keycode);
+
+        #endregion
     }
 }
